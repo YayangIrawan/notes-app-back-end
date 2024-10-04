8449934efd805d9f1ecb232b23e8c8342d43b060
Yayang Irawan <yayangirawan1297@gmail.com>
2024-10-04 18:34:07 +0700

    fix bugs host value

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 const init = async () => {
   const server = Hapi.server({
     port: 8000,
-    host: 'localhost',
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     //penerapan CORS pada config pembuatan server (u/ seluruh route)
     routes: {
       cors: {
